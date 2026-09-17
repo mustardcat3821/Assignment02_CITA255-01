00355ee114563a91c87ec94c79b7d87d52b7e010
mustardcat3821 <isaiahmccorkle57@gmail.com>
2026-09-17 17:20:56 -0400

    Project Readded
    File was corrupted

---
+++ Assignment2_AListOfThings/Assignment2_AListOfThings/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2_AListOfThings
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
